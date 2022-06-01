70386d57e2ef01aa78db58bf24ac6c2ffaa2bf0e
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-06-01 22:15:05 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+ void main()=> runApp(const MyApp());
+
+ class MyApp extends StatelessWidget {
+   final String data = '78293';
+   const MyApp({Key? key}) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+     return Provider<String>(
+       create: (context)=>data,
+       child: MaterialApp(
+         home: Scaffold(
+           appBar: AppBar(
+             centerTitle: true,
+             title: Text('Top Secret Code'),
+           ),
+           body: Level1(),
+         ),
+       ),
+     );
+   }
+ }
+
+ class Level1 extends StatelessWidget {
+
+   const Level1({Key? key}) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Container(
+       child: Level2(),
+     );
+   }
+ }
+ 
+ class Level2 extends StatelessWidget {
+
+   const Level2({Key? key}) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Container(
+       child: Level3(),
+     );
+   }
+ }
+ 
+ class Level3 extends StatelessWidget {
+
+   const Level3({Key? key}) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Center
+       (
+         child: Text(Provider.of<String>(context),
+         ),
+     );
+   }
+ }
+ 
